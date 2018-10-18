a9dd79977636785527f5eaf5eb91555d9cbefe1d
Xyman404 <44235910+Xyman404@users.noreply.github.com>
2018-10-18 17:46:54 +0300

    Add files via upload
    add some bugs

--- domaincheker/static/js/custom.js
+++ domaincheker/static/js/custom.js
@@ -1,19 +1,22 @@
 // Отправка заявки
 $(document).ready(function() {
     $('#check-domain-form').submit(function() {
-        if (document.form.name.value == "") {
+        if (document.form.value == "") {
             valid = false;
             return valid;
         }
+        $("#loader").show("slow");
         $.ajax({
             type: "POST",
             url: "http://127.0.0.1:3000/log",
             data: $(this).serialize()
+
         }).done(function(data) {
             console.log(data);
-            $("#result").text(data);
+            $("#result").html("<p>" + data + "</p>");
             $(this).find('input').val('');
             $('#check-domain-form').trigger('reset');
+            $("#loader").hide("slow");
         });
         return false;
     });
