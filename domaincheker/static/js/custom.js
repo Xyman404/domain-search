eeb19a2ba50fc250a445c07c36f21e0ae5465108
Xyman404 <44235910+Xyman404@users.noreply.github.com>
2018-10-23 11:11:58 +0300

    Update custom.js

--- domaincheker/static/js/custom.js
+++ domaincheker/static/js/custom.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
         $.ajax({
             url:     "http://127.0.0.1:3000/log", //url страницы
             type:     "POST", //метод отправки
-            data: $(this).serialize(),  // Сеарилизуем объект
+            data: $("#input-domain").serialize(),  // Сеарилизуем объект
         }).done(function(data){
             console.log(data);
-            $("#result").html("<p>" + data + "</p>");
+            $("#result").html(data);
             $(this).find('input').val('');
             $('#check-domain-form').trigger('reset');
             $("#loader").hide("slow");
